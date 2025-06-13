bbe192ad52bab6cc2f54def2ad818e6b383215b7
Davi Campoi <141076821+DaviCampoi@users.noreply.github.com>
2025-06-13 20:41:54 -0300

    Arrumar /app

--- App/app.js
+++ App/app.js
@@ -2,7 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import alunoRoutes from './routes/alunoRoutes.js';
 import atividadeRoutes from './routes/atividadeRoutes.js';
-const chatbotRoutes = require('./routes/chatbotRoutes.js');
+import chatbotRoutes from './routes/chatbotRoutes.js';
 import presencaRoutes from './routes/presencaRoutes.js';
 import responsavelRoutes from './routes/responsavelRoutes.js';
 import turmaRoutes from './routes/turmaRoutes.js';
